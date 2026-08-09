3278dc329817420a40e3dc5a58e0e514d1f74d76
leesolgi <sughjd9@gmail.com>
2026-08-09 22:09:55 +0900

    feat: 콘텐츠 찜하기 토글 기능 및 테스트 추가 (#1)
    * feat: 콘텐츠 찜하기 토글 기능 및 테스트 추가
    
    * fix: player-screen div/video 태그 중복 삽입으로 깨진 마크업 정리

--- pages/home_page.py
+++ pages/home_page.py
@@ -6,6 +6,7 @@ class HomePage(BasePage):
     KEYWORD_INPUT = (By.ID, 'keyword')
     SEARCH_BTN = (By.XPATH, "//button[text()='검색']")
     CONTENT_LIST = (By.ID, 'contentList')
+    LIKE_BTN_XPATH = "//li[contains(., '{title}')]//button[contains(@class,'like-btn')]"
 
     def search(self, keyword):
         self.type_text(self.KEYWORD_INPUT, keyword)
@@ -19,3 +20,12 @@ class HomePage(BasePage):
             lambda d: d.find_element(By.XPATH, f"//li[contains(., '{title_keyword}')]")
         )
         item.click()
+
+    def get_like_button(self, title_keyword):
+        return self.driver.find_element(By.XPATH, self.LIKE_BTN_XPATH.format(title=title_keyword))
+
+    def toggle_like(self, title_keyword):
+        self.get_like_button(title_keyword).click()
+
+    def is_liked(self, title_keyword):
+        return "liked" in self.get_like_button(title_keyword).get_attribute("class")
